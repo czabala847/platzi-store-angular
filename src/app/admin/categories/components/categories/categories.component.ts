87a5beb24b400ba98b0faee2d8f05933983ec92f
Carlos Zabala <czabala847@gmail.com>
2022-06-24 13:51:03 -0500

    Enviando imagen de categoria a Firebase

--- src/app/admin/categories/components/categories/categories.component.ts
+++ src/app/admin/categories/components/categories/categories.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/core/models/category.models';
+import { CategoriesService } from 'src/app/core/services/categories.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
+  categories: Category[] = [];
+  displayedColumns: string[] = ['id', 'name', 'image'];
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
+    this.getAllCategories();
   }
 
+  private getAllCategories() {
+    this.categoriesService
+      .getAll()
+      .subscribe((data) => (this.categories = data));
+  }
 }
--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Category } from 'src/app/core/models/category.models';
+import { CategoriesService } from 'src/app/core/services/categories.service';
+import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
-  styleUrls: ['./category-form.component.scss']
+  styleUrls: ['./category-form.component.scss'],
 })
 export class CategoryFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoriesService: CategoriesService,
+    private router: Router,
+    private storage: AngularFireStorage
+  ) {
+    this.buildForm();
   }
 
+  ngOnInit(): void {}
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      image: ['', Validators.required],
+    });
+  }
+
+  save() {
+    if (this.form.valid) {
+      this.createCategory();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private createCategory() {
+    const dto: Category = this.form.value;
+    this.categoriesService.create(dto).subscribe((response) => {
+      console.log(response);
+      this.router.navigate(['./admin/categories']);
+    });
+  }
+
+  handleUpload(event) {
+    const element = event.target as HTMLInputElement;
+    const image = element.files[0];
+    const name = 'category.png';
+    const ref = this.storage.ref(name);
+    const task = this.storage.upload(name, image);
+
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          const urlImage$ = ref.getDownloadURL();
+          urlImage$.subscribe((url) => {
+            console.log(url);
+            this.imageField.setValue(url);
+          });
+        })
+      )
+      .subscribe();
+  }
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
+  get imageField() {
+    return this.form.get('image');
+  }
 }
